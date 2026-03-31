b57395620ae3ae2f5384d819a23a76d185958165
prettym-prog <prettym@oregonstate.edu>
2026-03-31 16:17:44 -0400

    animal.py
    This script prompts the user for their favorite animal and prints it.

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Mikyra Pretty
+# GitHUb username: prettym-prog
+# Date: 3/31/2026
+# Description: Asks the user for their favorite animal and then 
+#              prints out that that is their favorite animal
+
+fave_animal = input("lion")
+print("Your favorite animal is the " + fave_animal + ".")
