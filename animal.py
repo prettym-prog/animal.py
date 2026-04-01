1a6d1fd010b01e8aa95f75626a7b553bd34f278e
prettym-prog <prettym@oregonstate.edu>
2026-03-31 21:26:32 -0400

    Update input prompt for favorite animal

--- animal.py
+++ animal.py
@@ -4,5 +4,5 @@
 # Description: Asks the user for their favorite animal and then 
 #              prints out that that is their favorite animal
 
-fave_animal = input("velociraptor")
+fave_animal = input("Please enter your favorite animal: ")
 print("Your favorite animal is the " + fave_animal + ".")
